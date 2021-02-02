c44d676f802731a7487f09f1a64906db21852852
Tapdiq <tapdigna@code.edu.az>
2021-02-02 15:49:13 +0400

    Update footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,17 @@ $(document).ready(function(){
             $("#back2Top").fadeOut();
         }
     });
+    $("#back2Top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 'slow');
+    });
+
+    $(".navbar-toggler").click(function () {
+        $(this).next().addClass('show');
+        $(".nav-overlay-panel").css('display', 'block');
+    });
+
+    $(".navbar-menus .close-button").click(function () {
+        $(this).parent().removeClass('show');
+        $(".nav-overlay-panel").css('display', 'none');
+    });
 });
