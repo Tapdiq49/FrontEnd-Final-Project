beae9a3dfd39e0e35e005c9a8b3f5c17bc92b751
Tapdiq <tapdigna@code.edu.az>
2021-02-03 22:44:46 +0400

    Add top-category section

--- Wep application/assets/js/main.js
+++ Wep application/assets/js/main.js
@@ -21,4 +21,21 @@ $(document).ready(function(){
         $(this).parent().removeClass('show');
         $(".nav-overlay-panel").css('display', 'none');
     });
+    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
 });
